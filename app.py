7fa3b1d01b5ed91e9d42a16f8e4386e7a748fa68
gutner <refromto@naver.com>
2022-05-09 18:17:20 +0900

    회원가입 페이지 기본 모양

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from pymongo import MongoClient
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 client = MongoClient('13.125.152.229', 27017, username="test", password="test")
 db = client.MyHiddenSong
 
+
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template('Sign_up_page.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
