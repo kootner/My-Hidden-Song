2787d384f93f5fa55a9e04e9e06b40a5c3c1723f
eank0108 <eank0108@naver.com>
2022-05-09 20:27:54 +0900

    추가 - 루트 랜더링 예외처리

--- app.py
+++ app.py
@@ -17,14 +17,22 @@ db = client.MyHiddenSong
 @app.route('/')
 def home():
     token = request.cookies.get('mytoken')
-    if token is not None:
-        payload = jwt.decode(token, TOKEN_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"id": payload["id"]})
-        if user_info is None:
-            return render_template('index.html', msg='로그인 정보가 없습니다.')
-
-        return render_template('list.html', user_info=user_info)
-    else:
+    print(token)
+    try:
+        if token is not None:
+            payload = jwt.decode(token, TOKEN_KEY, algorithms=['HS256'])
+            user_info = db.users.find_one({"id": payload["id"]})
+            if user_info is None:
+                return render_template('index.html', msg='로그인 정보가 없습니다.')
+    
+            return render_template('list.html', user_info=user_info)
+        else:
+            return render_template('index.html')
+    except jwt.ExpiredSignatureError:
+        print("로그인 시간 만료")
+        return render_template('index.html')
+    except jwt.exceptions.DecodeError:
+        print("로그인 정보가 없습니다")
         return render_template('index.html')
 
 
@@ -33,15 +41,16 @@ def sign_in():
     id = request.form['id']
     pw = request.form['pw']
     pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+
     result = db.users.find_one({'id': id, 'pw': pw_hash})
 
     if result is not None:
         # 입력받은 id, pw 와 일치하는 유저가 db에 존재할때
         payload = {
             'id': id,
-            'expire': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60)  # 로그인 24시간 유지
         }
-        token = jwt.encode(payload, TOKEN_KEY, algorithm='HS256').decode('utf-8')
+        token = jwt.encode(payload, TOKEN_KEY, algorithm='HS256')
         return jsonify({'result': 'success', 'token': token})
     else:
         return jsonify({'result': 'fail', 'msg': "로그인 실패!"})
