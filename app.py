2f5fea112b6e6d33e3f9f2d4081e2c4f8abfd497
gutner <refromto@naver.com>
2022-05-10 12:05:48 +0900

    로그인 기능 구현

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from flask import Flask, request, jsonify, render_template, redirect
+from flask import Flask, request, jsonify, render_template, redirect,url_for
 import hashlib
 import jwt
 from datetime import datetime, timedelta
@@ -25,7 +25,7 @@ def home():
             if user_info is None:
                 return render_template('index.html', msg='로그인 정보가 없습니다.')
 
-            return render_template('list.html', user_info=user_info)
+            return render_template('music_list.html', user_info=user_info)
         else:
             return render_template('index.html')
     except jwt.ExpiredSignatureError:
@@ -35,19 +35,24 @@ def home():
         print("로그인 정보가 없습니다")
         return render_template('index.html')
 
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('index.html', msg=msg)
+
 
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
-    id = request.form['id']
-    pw = request.form['pw']
-    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
 
-    result = db.users.find_one({'id': id, 'pw': pw_hash})
+    result = db.users.find_one({'id': id_receive, 'pw': pw_hash})
 
     if result is not None:
         # 입력받은 id, pw 와 일치하는 유저가 db에 존재할때
         payload = {
-            'id': id,
+            'id': id_receive,
             'exp': datetime.utcnow() + timedelta(seconds=60 * 60)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, TOKEN_KEY, algorithm='HS256')
