fc1b93d340d8d6fef947830d6d77fe0c93dba9d1
gygy2022 <2022gygy@gmail.com>
2022-05-10 22:06:30 +0900

    로그아웃 버튼 제이쿼리 추가, 로그아웃 함수 추가

--- app.py
+++ app.py
@@ -200,6 +200,12 @@ def getYoutubeUrl():
 
 # Youtube Branch End
 
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+
+    return render_template('index.html', msg=msg)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
