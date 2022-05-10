672bc9775d16e3eb998c6c552e93860fae55857f
gutner <refromto@naver.com>
2022-05-10 10:22:12 +0900

    노래 리스트 카드 내용표현 기능 구현

--- app.py
+++ app.py
@@ -44,5 +44,10 @@ def sign_up():
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
+@app.route('/music_list/data', methods=['GET'])
+def music_data():
+    music_data = list(db.musics.find({}, {'_id': False}))
+    return jsonify({'all_music': music_data})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
