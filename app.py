0f24cd50850ad90a78e2908e2e33ce3e5343ea76
eank0108 <eank0108@naver.com>
2022-05-11 12:32:13 +0900

    추가 - 검색 시스템

--- app.py
+++ app.py
@@ -266,6 +266,13 @@ def login():
 
     return render_template('index.html', msg=msg)
 
+@app.route('/search', methods=['GET'])
+def search():
+    search_text = request.args.get("search_text")
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get("https://www.genie.co.kr/search/searchAuto?query="+search_text, headers=headers)
+    return jsonify(data.json())
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
